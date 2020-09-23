5d8c1a8fbddba55774d3fb867382a191800ea19e
Irving Rafael <irving.ruvalcaba5700@alumnos.udg.mx>
2020-09-22 21:59:03 -0500

    Hicimos mezcla desde prueba

---
+++ Funciones.h
@@ -0,0 +1,14 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+void suma(int a, int b){
+    return a+b;
+}
+void resta(int a, int b){
+    if (b < 0){
+        return b;
+
+    }
+    return a-b;
+}
+#endif // FUNCIONES_H
